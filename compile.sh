916db5395643d908abbeeb93a0d2a17db852c312
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-05-20 21:32:39 +0200

    forget browsers

--- compile.sh
+++ compile.sh
@@ -1,17 +1,5 @@
 cd lua/src
 make emscripten
 
-cat <<EOT >> lua.js
-FS_stdin_getChar = function () {
-    let inputBuffer = Buffer(luaInput)
-    luaInput = ''
-
-    if (inputBuffer.length === 0) {
-        return null; // No input available
-    }
-    return inputBuffer.pop().charCodeAt(0);
-};
-EOT
-
 cp lua.js ../../dist/
 cp lua.wasm ../../dist/
